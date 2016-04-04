b21114f46858bdf3e82eacaad03fb73c909007ad
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-04-04 14:39:00 +0300

    Docs and pragmas

--- library/Interspersed.hs
+++ library/Interspersed.hs
@@ -9,22 +9,31 @@ where
 import Interspersed.Prelude
 
 
+-- |
+-- An abstraction over interspersing monadic actions.
 newtype Interspersed m a =
   Interspersed (ReaderT (m ()) (StateT Bool m) a)
   deriving (Functor, Applicative, Alternative, Monad, MonadPlus)
 
 instance MonadTrans Interspersed where
+  {-# INLINE lift #-}
   lift =
     interspersed
 
+{-# INLINE runInterspersed #-}
 runInterspersed :: Monad m => Interspersed m a -> m () -> m a
 runInterspersed (Interspersed impl) sep =
+  {-# SCC "runInterspersed" #-} 
   flip evalStateT True $
   flip runReaderT sep $
   impl
 
+-- |
+-- Lifts a monadic action. Same as 'lift'.
+{-# INLINABLE interspersed #-}
 interspersed :: Applicative m => m a -> Interspersed m a
 interspersed fx =
+  {-# SCC "interspersed" #-} 
   Interspersed $
   ReaderT $
   \sep ->
